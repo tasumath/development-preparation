5a4b586a5224af1f446887f05e9b17128132d07d
maehiro <117615272+tasumath@users.noreply.github.com>
2023-01-12 23:12:30 +0900

    Add files via upload

--- saizeriya_select_c(edit).c
+++ saizeriya_select_c(edit).c
@@ -6,6 +6,8 @@
 struct menu{
     char name[100];
     int value;
+    int calorie;
+    double salt_amount;
     int menu_number;
 };
 
@@ -14,34 +16,34 @@ int main(){
     clock_t start,end;
     start = clock();
 
-    menu m0 = {"小エビのサラダ",350,0};
-    menu m1 = {"チキンのシーザーサラダ",350,1};
-    menu m2 = {"ガーデンサラダ",350,2};
-    menu m3 = {"コーンクリームスープ",150,3};
-    menu m4 = {"白菜のクラムチャウダー",300,4};
-    menu m5 = {"田舎風ミネストローネ",300,5};
-    menu m6 = {"プチフォッカ",150,6};
-    menu m7 = {"セットプチフォッカ",100,7};
-    menu m8 = {"ほうれん草のくたくた",300,8};
-    menu m9 = {"爽やかにんじんサラダ",200,9};
-    menu m10 = {"ペコリーノ・ロマーノ",100,10};
-    menu m11 = {"ビール",400,11};
-    menu m12 = {"シナモンプチフォッカ",200,12};
-    menu m13 = {"ミニフィセル",150,13};
-    menu m14 = {"ガーリックトースト",150,14};
-    menu m15 = {"ブロッコリーのくたくた",300,15};
-    menu m16 = {"柔らか青豆とペコリーノチーズの温サラダ",200,16};
-    menu m17 = {"バッファローモッツァレラ",300,17};
-    menu m18 = {"辛味チキン",300,18};
-    menu m19 = {"カリットポテト",250,19};
-    menu m20= {"ポップコーンシュリンプ",300,20};
-    menu m21 = {"煉獄のたまご",300,21};
-    menu m22 = {"イタリア風もつ煮込み",350,22};
-    menu m23 = {"エスカルゴのオーブン焼き",400,23};
-    menu m24 = {"アロスティチーニ",400,24};
-    menu m25= {"チョリソー",400,25};
-    menu m26 = {"ムール貝のガーリック焼き",400,26};
-    menu m27 = {"エビクリームグラタン",400,27};
+    menu m0 = {"小エビのサラダ",350,127,1.4,0};
+    menu m1 = {"チキンのシーザーサラダ",350,203,1.3,1};
+    menu m2 = {"ガーデンサラダ",350,173,0.7,2};
+    menu m3 = {"コーンクリームスープ",150,146,1.1,3};
+    menu m4 = {"白菜のクラムチャウダー",300,273,1.6,4};
+    menu m5 = {"田舎風ミネストローネ",300,178,2.2,5};
+    menu m6 = {"プチフォッカ",150,214,0.8,6};
+    menu m7 = {"セットプチフォッカ",100,107,0.4,7};
+    menu m8 = {"ほうれん草のくたくた",300,197,1.7,8};
+    menu m9 = {"爽やかにんじんサラダ",200,93,0.9,9};
+    menu m10 = {"ペコリーノ・ロマーノ",100,78,0.8,10};
+    menu m11 = {"ビール",400,160,0,11};
+    menu m12 = {"シナモンプチフォッカ",200,246,0.8,12};
+    menu m13 = {"ミニフィセル",150,189,1.0,13};
+    menu m14 = {"ガーリックトースト",150,245,1.1,14};
+    menu m15 = {"ブロッコリーのくたくた",300,125,1.1,15};
+    menu m16 = {"柔らか青豆とペコリーノチーズの温サラダ",200,199,1.0,16};
+    menu m17 = {"バッファローモッツァレラ",300,209,0.5,17};
+    menu m18 = {"辛味チキン",300,295,1.8,18};
+    menu m19 = {"カリットポテト",250,389,2.1,19};
+    menu m20= {"ポップコーンシュリンプ",300,215,1.4,20};
+    menu m21 = {"煉獄のたまご",300,319,2.0,21};
+    menu m22 = {"イタリア風もつ煮込み",350,202,0.8,22};
+    menu m23 = {"エスカルゴのオーブン焼き",400,220,1.3,23};
+    menu m24 = {"アロスティチーニ",400,224,0.6,24};
+    menu m25= {"チョリソー",400,425,2.9,25};
+    menu m26 = {"ムール貝のガーリック焼き",400,170,1.4,26};
+    menu m27 = {"エビクリームグラタン",400,505,2.0,27};
 
     struct menu* data[] = {&m0,&m1,&m2,&m3,&m4,&m5,&m6,&m7,&m8,&m9,&m10,&m11,&m12,&m13,&m14,&m15,&m16,&m17,&m18,&m19,&m20,&m21,&m22,&m23,&m24,&m25,&m26,&m27};
     int len = sizeof(data) / sizeof(data[0]);
@@ -96,13 +98,14 @@ int main(){
     remaining_amount = goal_value - (Answer_value_1 + Answer_value_2);
 
     //ここの条件を商品を追加するたびに変える
-    if(100 < remaining_amount && remaining_amount <= 400) select_passed = true;
+    if(100 <= remaining_amount && remaining_amount <= 400) select_passed = true;
     else continue;
 
     int koho_number[27];
 
     for(int i=0;i<len;i++){
         if(menu_cnt[i] == 0 && remaining_amount == data[i]->value){
+            //printf("%s:%d\n",data[i]->name,data[i]->value);
             koho_number[i] =  data[i]->menu_number;
         }
         else{
@@ -130,13 +133,17 @@ int main(){
 
     for(int i=0;i<len;i++){
         if(koho_number[i] > 0){
+            //printf("%d:::%d\n",i,koho_number[i]);
             decided_array[j] = koho_number[i];
+            //printf("%d",decided_array[j]);
             j--;
         }
     }
     
     srand(time(NULL));
-    int index3 = rand() % cnt;
+    int index3 = rand() % (cnt+1);
+    index3 = decided_array[index3];
+    //printf("%d",index3);
     Answer_number_3 = data[index3]->menu_number;
     //Answer_value_3 = data[index3]->value;
 
@@ -145,8 +152,9 @@ int main(){
 
     printf("%d円で3品を選びました!\n",goal_value);
 
-    printf("1品目は%sです.2品目は%sです.3品目は%sです.\n",data[Answer_number_1]->name,data[Answer_number_2]->name,data[Answer_number_3]->name);
-
+    printf("1品目は%sです.値段は%d円,カロリーは%dkcal,塩分は%.1fgです.\n",data[Answer_number_1]->name,data[Answer_number_1]->value,data[Answer_number_1]->calorie,data[Answer_number_1]->salt_amount);
+    printf("2品目は%sです.値段は%d円,カロリーは%dkcal,塩分は%.1fgです.\n",data[Answer_number_2]->name,data[Answer_number_2]->value,data[Answer_number_2]->calorie,data[Answer_number_2]->salt_amount);
+    printf("3品目は%sです.値段は%d円,カロリーは%dkcal,塩分は%.1fgです.\n",data[Answer_number_3]->name,data[Answer_number_3]->value,data[Answer_number_3]->calorie,data[Answer_number_3]->salt_amount);
     end = clock();
 
     float time = (float)(end - start) / CLOCKS_PER_SEC;
